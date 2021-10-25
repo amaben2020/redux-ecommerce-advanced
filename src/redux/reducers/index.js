8effc676660a2a0dc88bc98334def607ad6701f6
amaben2020 <amaben202phantom@gmail.com>
2021-10-25 18:22:34 +0100

    Coupon added

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,9 +3,11 @@ import { userReducer } from './userReducer.ts';
 import { searchReducer } from './searchReducer.ts';
 import { cartReducer } from './cartReducer.js';
 import { drawerReducer } from './drawerReducer';
+import { couponReducer } from './couponReducer';
 export const rootReducer = combineReducers({
   user: userReducer,
   search: searchReducer,
   cart: cartReducer,
   drawer: drawerReducer,
+  coupon: couponReducer,
 });
